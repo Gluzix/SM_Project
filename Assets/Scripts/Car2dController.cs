488b46402f539ad5353b3a9169f8c68c3916b6bc
Kamilen <kamil.gluza@gmail.com>
2020-04-22 20:32:28 +0200

    Added Car, Racetrack, world psyhics, car psychics

---
+++ Assets/Scripts/Car2dController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car2dController : MonoBehaviour
+{
+    //zmienna speedForce określa jaka siła zostanie dodana do wektora up
+    float speedForce = 10f;
+    float brakeForce = -50f;
+    float torqueForce = 400f;
+    float driftFactorSticky = 0.9f;
+    float driftFactorSlippy = 1f;
+    float maxStickyVelocity = 2.5f;
+    float minSlippyVelocity = 1.5f;
+    float speedTuning = 3.0f;
+    public float roadStickness = 1.0f;
+    GameObject roadPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roadPos = GameObject.Find("Asphalt");
+        //52:36 - tutorial stopped
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          
+    }
+
+    void FixedUpdate()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        float driftFactor = driftFactorSticky;
+
+       /* if ( gameObject.transform.position.x > ( roadPos.transform.position.x) - ( roadPos.GetComponent<MeshRenderer>().bounds.size.x / 2 ) )
+        {
+            roadStickness = 1.0f;
+        }
+        else
+        {
+            roadStickness = 0.5f;
+        } */     
+
+        if (RightVelocity().magnitude > maxStickyVelocity)
+        {
+            driftFactor = driftFactorSlippy;
+        }
+
+        rb.velocity = ForwardVelocity() + RightVelocity() * driftFactorSlippy;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(transform.up * speedForce * speedTuning * roadStickness);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(transform.up * brakeForce);
+        }
+
+
+        float tf = Mathf.Lerp(0, torqueForce, rb.velocity.magnitude / 4 );
+
+
+        rb.angularVelocity = Input.GetAxis("Horizontal") * tf;
+
+
+
+        Debug.Log(rb.velocity.magnitude*5);
+    }
+
+    Vector2 ForwardVelocity()
+    {
+        return transform.up * Vector2.Dot( GetComponent<Rigidbody2D>().velocity, transform.up );
+    }
+
+    Vector2 RightVelocity()
+    {
+        return transform.right * Vector2.Dot(GetComponent<Rigidbody2D>().velocity, transform.right );
+    }
+}
