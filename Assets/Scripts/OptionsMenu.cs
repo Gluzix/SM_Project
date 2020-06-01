2e60f3adddb4c5ab042e1605c398a1c845574131
Kamilen <kamil.gluza@gmail.com>
2020-06-01 23:27:22 +0200

    working on new track_3, addded missing music files and assets

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public static float InGameVolume = 1f;
+    public static string DifficultyLevel = "Medium";
+
+    void Start()
+    {
+        Slider volumeSlider = GameObject.Find("VolumeSlider").GetComponent<Slider>();
+        volumeSlider.value = InGameVolume;
+        volumeSlider.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = ((int)(InGameVolume * 100)).ToString() + "%";
+        GameObject.Find("DifficultyButton").GetComponentInChildren<TMPro.TextMeshProUGUI>().text = DifficultyLevel;
+    }
+
+    public void ChangeDifficultyLevel()
+    {
+        if( DifficultyLevel == "Easy" )
+        {
+            DifficultyLevel = "Medium";
+        }
+        else if (DifficultyLevel == "Medium")
+        {
+            DifficultyLevel = "Hard";
+        }
+        else if( DifficultyLevel == "Hard" )
+        {
+            DifficultyLevel = "Unbeatable";
+        }
+        else
+        {
+            DifficultyLevel = "Easy";
+        }
+
+        Difficulty.SetDifficulty(DifficultyLevel);
+        GameObject.Find("DifficultyButton").GetComponentInChildren<TMPro.TextMeshProUGUI>().text = DifficultyLevel;
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void ChangeVolumeLevel()
+    {
+        Slider volumeSlider = GameObject.Find("VolumeSlider").GetComponent<Slider>();
+        InGameVolume = volumeSlider.value;
+        volumeSlider.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = ((int)(InGameVolume * 100)).ToString() + "%";
+        AudioSource gameAudio = GameObject.Find("MusicPlayer").GetComponent<AudioSource>();
+        gameAudio.volume = InGameVolume;
+    }
+}
